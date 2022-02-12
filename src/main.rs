69003744f42c6ea89bb48c79fbd84d2e8a841891
Night Cruising <2586447362@qq.com>
2022-02-12 23:45:30 +0800

    Program entry

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+use gloo::storage::{LocalStorage, Storage};
+use serde::{Deserialize, Serialize};
+use web_sys::HtmlInputElement;
+use yew::{html, html::Scope, Component, Context, Html, KeyboardEvent, TargetCast};
+
+mod footer;
+mod header;
+mod todo;
+
+use footer::Footer;
+use header::Header;
+use todo::Todo;
+
+struct App;
+
+impl Component for App {
+    type Message = ();
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        App
+    }
+
+    fn view(&self, _ctx: &Context<Self>) -> Html {
+        html! {
+            <>
+                <Header></Header>
+                <br/>
+                <br/>
+                <div class="container text-align-center" style="max-width: 600px">
+                    <Todo></Todo>
+                    <br/>
+                    <br/>
+                    <Footer></Footer>
+                </div>
+            </>
+        }
+    }
+}
+
 fn main() {
-	println!("Hello World");
+    yew::start_app::<App>();
 }
